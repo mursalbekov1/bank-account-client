cea5fe62fa15e8cd96d10b53f470ffbfb744c821
mursalbekov1 <mursalbekov.merey@gmail.com>
2024-05-08 00:34:48 +0500

    update: url string

--- src/main/java/com/example/bankaccountclient/controller/ActivityController.java
+++ src/main/java/com/example/bankaccountclient/controller/ActivityController.java
@@ -2,27 +2,24 @@ package com.example.bankaccountclient.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
-@RequestMapping("/activity")
 @RestController
 public class ActivityController {
 
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
     public ActivityController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
-    @GetMapping
+    @GetMapping("/activity")
     @CircuitBreaker(name = "randomActivity", fallbackMethod = "fallbackRandomActivity")
     public String getRandomActivity() {
-        String response = restTemplate.getForObject("http://localhost:8080/listUser", String.class);
+        String response = restTemplate.getForObject("http://localhost:8086/listUser", String.class);
         return response;
     }
 
